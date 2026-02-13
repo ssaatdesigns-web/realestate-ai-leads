af19eccc29be7deacd025ffe77817a6f450a6f0d
ssaatdesigns-web <ssaatdesigns@gmail.com>
2026-02-13 22:14:31 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { supabaseAdmin } from "@/lib/supabaseAdmin";
+import { supabaseAdmin } from "../lib/supabaseAdmin";
 
 export default async function Home() {
   const { data: leads, error } = await supabaseAdmin
