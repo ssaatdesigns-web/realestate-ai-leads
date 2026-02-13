6653fe04ea3fe99952e43fdb4792f322bc350e51
ssaatdesigns-web <ssaatdesigns@gmail.com>
2026-02-13 22:03:10 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  experimental: {
-    serverActions: true
-  },
-
-  typescript: {
-    ignoreBuildErrors: false
-  },
-
-  eslint: {
-    ignoreDuringBuilds: false
-  }
 };
 
 module.exports = nextConfig;
